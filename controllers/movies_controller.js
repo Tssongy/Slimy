966e12dcd01b9a913da18eb5330dd62d54940bcc
oscar <opawong@yahoo.com>
2022-03-17 00:33:45 +1100

    movie-detail page

--- controllers/movies_controller.js
+++ controllers/movies_controller.js
@@ -3,4 +3,21 @@ const Movie = require("../models/movie");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {});
+router.get("/:title", (req, res) => {
+  const title = req.params.title;
+  console.log("hello");
+  Movie.findSearchResults(title).then((movies) => res.json(movies));
+  //   const title = req.params.id;
+  //   Movie.findSearchResults(title).then((movies) => res.json(movies));
+});
+
+router.post("/", (req, res) => {
+  const title = req.body.Title;
+  const poster = req.body.Poster;
+  const actors = req.body.Actors;
+  const description = req.body.Plot;
+
+  Movie.create(title, actors, poster, description);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
-// const logger =  require('./middlewares/logger')
-// const sessions =  require('./middlewares/sessions')
+const logger = require("./middlewares/logger");
+// const sessions = require("./middlewares/sessions");
 
-// const moviesController = require('./controllers/movies_controller')
+const moviesController = require("./controllers/movies_controller");
 // const usersController = require('./controllers/users_controller')
 // const sessionsController = require('./controllers/sessions_controller')
 
@@ -12,14 +12,14 @@ const port = 3000;
 
 app.listen(port, () => console.log(`server listening on port ${port}`));
 
-// app.use(logger)
+app.use(logger);
 
 app.use(express.static("client"));
 
 app.use(express.json());
 
-// app.use(sessions)
+// app.use(sessions);
 
-// app.use('/api/movies',moviesController)
+app.use("/api/movies", moviesController);
 // app.use('/api/users',usersController)
 // app.use('/api/sessions',sessionsController)
