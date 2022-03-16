f74c23897d68d0a62f8af381cfd10f9a8a3bbcf8
oscar <opawong@yahoo.com>
2022-03-16 16:27:18 +1100

    working

--- client/js/components/movie_list.js
+++ client/js/components/movie_list.js
@@ -3,16 +3,33 @@ function renderMovieList() {
   axios
     .get(`https://omdbapi.com?apikey=2f6435d9&s=${movieTitle}`)
     .then((res) => {
-        const movieList = res.data.Search.map(movie => {
-            const title = movie.Title 
-            const posterUrl = movie.Poster
-            return `
+      const movieList = res.data.Search.map((movie) => {
+        const title = movie.Title;
+        const posterUrl = movie.Poster;
+        const imdbId = movie.imdbId;
+        return `
               <div>
               <h6>${title}</h6>
-              <img src="${posterUrl}"></img>
+              <img src="${posterUrl}" onClick="renderMovieDetail(${imdbId})"></img>
               </div>
-            `
-        }).join("")
-        document.querySelector('#page').innerHTML = movieList
-    })
+            `;
+      }).join("");
+      document.querySelector("#page").innerHTML = movieList;
+    });
+}
+
+function renderMovieDetail(imdbId) {
+  axios.get(`https://omdbapi.com?apikey=2f6435d9&i=${imdbId}`).then((res) => {
+    const movie = res;
+    console.log(res);
+    const movieDetail = `
+        <div>
+          <img src="${movie.Poster}"></img>
+        </div>
+        <div>
+          <h4>${movie.Title}</h4>
+          <h6></h6>
+        </div>
+      `;
+  });
 }
